c54bd98b998e62047b5eb84fdfabccefe66667bc
Steen Gundersborg <steen@teletronics.ae>
2017-06-05 10:31:08 +0400

    EDR-3343 - Reverted our changes to a test, since stock now works

--- solr/core/src/test/org/apache/solr/rest/schema/TestFieldTypeResource.java
+++ solr/core/src/test/org/apache/solr/rest/schema/TestFieldTypeResource.java
@@ -23,61 +23,63 @@ public class TestFieldTypeResource extends SolrRestletTestBase {
   @Test
   public void testGetFieldType() throws Exception {
     assertQ("/schema/fieldtypes/float?indent=on&wt=xml&showDefaults=true",
-            "count(/response/lst[@name='fieldType']) = 1",
-            "count(/response/lst[@name='fieldType']/*) = 16",
-            "/response/lst[@name='fieldType']/str[@name='name'] = 'float'",
-            "/response/lst[@name='fieldType']/str[@name='class'] = 'solr.TrieFloatField'",
-            "/response/lst[@name='fieldType']/str[@name='precisionStep'] ='0'",
-            "/response/lst[@name='fieldType']/bool[@name='indexed'] = 'true'",
-            "/response/lst[@name='fieldType']/bool[@name='stored'] = 'true'",
-            "/response/lst[@name='fieldType']/bool[@name='docValues'] = 'false'",
-            "/response/lst[@name='fieldType']/bool[@name='termVectors'] = 'false'",
-            "/response/lst[@name='fieldType']/bool[@name='termPositions'] = 'false'",
-            "/response/lst[@name='fieldType']/bool[@name='termOffsets'] = 'false'",
-            "/response/lst[@name='fieldType']/bool[@name='omitNorms'] = 'true'",
-            "/response/lst[@name='fieldType']/bool[@name='omitTermFreqAndPositions'] = 'true'",
-            "/response/lst[@name='fieldType']/bool[@name='omitPositions'] = 'false'",
-            "/response/lst[@name='fieldType']/bool[@name='storeOffsetsWithPositions'] = 'false'",
-            "/response/lst[@name='fieldType']/bool[@name='multiValued'] = 'false'",
-            "/response/lst[@name='fieldType']/bool[@name='tokenized'] = 'false'");
+        "count(/response/lst[@name='fieldType']) = 1",
+        "count(/response/lst[@name='fieldType']/*) = 16",
+        "/response/lst[@name='fieldType']/str[@name='name'] = 'float'",
+        "/response/lst[@name='fieldType']/str[@name='class'] = 'solr.TrieFloatField'",
+        "/response/lst[@name='fieldType']/str[@name='precisionStep'] ='0'",
+        "/response/lst[@name='fieldType']/bool[@name='indexed'] = 'true'",
+        "/response/lst[@name='fieldType']/bool[@name='stored'] = 'true'",
+        "/response/lst[@name='fieldType']/bool[@name='docValues'] = 'false'",
+        "/response/lst[@name='fieldType']/bool[@name='termVectors'] = 'false'",
+        "/response/lst[@name='fieldType']/bool[@name='termPositions'] = 'false'",
+        "/response/lst[@name='fieldType']/bool[@name='termOffsets'] = 'false'",
+        "/response/lst[@name='fieldType']/bool[@name='omitNorms'] = 'true'",
+        "/response/lst[@name='fieldType']/bool[@name='omitTermFreqAndPositions'] = 'true'",
+        "/response/lst[@name='fieldType']/bool[@name='omitPositions'] = 'false'",
+        "/response/lst[@name='fieldType']/bool[@name='storeOffsetsWithPositions'] = 'false'",
+        "/response/lst[@name='fieldType']/bool[@name='multiValued'] = 'false'",
+        "/response/lst[@name='fieldType']/bool[@name='tokenized'] = 'false'");
   }
 
   @Test
   public void testGetNotFoundFieldType() throws Exception {
     assertQ("/schema/fieldtypes/not_in_there?indent=on&wt=xml",
-            404, "Field type 'not_in_there' not found");
+        "count(/response/lst[@name='fieldtypes']) = 0",
+        "/response/lst[@name='responseHeader']/int[@name='status'] = '404'",
+        "/response/lst[@name='error']/int[@name='code'] = '404'");
   }
 
   @Test
   public void testJsonGetFieldType() throws Exception {
     assertJQ("/schema/fieldtypes/float?indent=on&showDefaults=on",  // assertJQ will add "&wt=json"
-             "/fieldType/name=='float'",
-             "/fieldType/class=='solr.TrieFloatField'",
-             "/fieldType/precisionStep=='0'",
-             "/fieldType/indexed==true",
-             "/fieldType/stored==true",
-             "/fieldType/docValues==false",
-             "/fieldType/termVectors==false",
-             "/fieldType/termPositions==false",
-             "/fieldType/termOffsets==false",
-             "/fieldType/omitNorms==true",
-             "/fieldType/omitTermFreqAndPositions==true",
-             "/fieldType/omitPositions==false",
-             "/fieldType/storeOffsetsWithPositions==false",
-             "/fieldType/multiValued==false",
-             "/fieldType/tokenized==false");
+        "/fieldType/name=='float'",
+        "/fieldType/class=='solr.TrieFloatField'",
+        "/fieldType/precisionStep=='0'",
+        "/fieldType/indexed==true",
+        "/fieldType/stored==true",
+        "/fieldType/docValues==false",
+        "/fieldType/termVectors==false",
+        "/fieldType/termPositions==false",
+        "/fieldType/termOffsets==false",
+        "/fieldType/omitNorms==true",
+        "/fieldType/omitTermFreqAndPositions==true",
+        "/fieldType/omitPositions==false",
+        "/fieldType/storeOffsetsWithPositions==false",
+        "/fieldType/multiValued==false",
+        "/fieldType/tokenized==false");
   }
-  
+
   @Test
   public void testGetFieldTypeDontShowDefaults() throws Exception {
     assertQ("/schema/fieldtypes/teststop?wt=xml&indent=on",
-            "count(/response/lst[@name='fieldType']/*) = 3",
-            "/response/lst[@name='fieldType']/str[@name='name'] = 'teststop'",
-            "/response/lst[@name='fieldType']/str[@name='class'] = 'solr.TextField'",
-            "/response/lst[@name='fieldType']/lst[@name='analyzer']/lst[@name='tokenizer']/str[@name='class'] = 'solr.LowerCaseTokenizerFactory'",
-            "/response/lst[@name='fieldType']/lst[@name='analyzer']/arr[@name='filters']/lst/str[@name='class'][.='solr.StandardFilterFactory']",
-            "/response/lst[@name='fieldType']/lst[@name='analyzer']/arr[@name='filters']/lst/str[@name='class'][.='solr.StopFilterFactory']",
-            "/response/lst[@name='fieldType']/lst[@name='analyzer']/arr[@name='filters']/lst/str[@name='words'][.='stopwords.txt']"
-            );
+        "count(/response/lst[@name='fieldType']/*) = 3",
+        "/response/lst[@name='fieldType']/str[@name='name'] = 'teststop'",
+        "/response/lst[@name='fieldType']/str[@name='class'] = 'solr.TextField'",
+        "/response/lst[@name='fieldType']/lst[@name='analyzer']/lst[@name='tokenizer']/str[@name='class'] = 'solr.LowerCaseTokenizerFactory'",
+        "/response/lst[@name='fieldType']/lst[@name='analyzer']/arr[@name='filters']/lst/str[@name='class'][.='solr.StandardFilterFactory']",
+        "/response/lst[@name='fieldType']/lst[@name='analyzer']/arr[@name='filters']/lst/str[@name='class'][.='solr.StopFilterFactory']",
+        "/response/lst[@name='fieldType']/lst[@name='analyzer']/arr[@name='filters']/lst/str[@name='words'][.='stopwords.txt']"
+    );
   }
 }
